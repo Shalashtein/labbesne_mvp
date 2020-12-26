8cca9c30be0a76dc232e37bcccab3ab79f4ba746
Abe <shalashtein@gmail.com>
2020-12-26 13:09:36 +0200

    tasks are being created

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -2,9 +2,8 @@ module Spree::CheckoutControllerDecorator
 
   def finalize_order
     @current_order.line_items.each do |item|
-      t = Track.new(spree_line_item_id: item.id, vendor_recieved: false, vendor_sent: false, recieved: false, quantity: item.quantity, spree_user_id: Spree::Product.find(Spree::Variant.find(t.variant_id)).spree_user_id)
+      t = Track.new(spree_line_item_id: item.id, vendor_recieved: false, vendor_sent: false, recieved: false, quantity: item.quantity, spree_user_id: Spree::Product.find(Spree::Variant.find(item.variant_id).product_id).spree_user_id)
       t.save!
-      puts "New task created"
     end
     @current_order = nil
     set_successful_flash_notice
