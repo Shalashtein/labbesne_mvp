b2dbf8fbfc15c58d89d12b538df41819c8a7c5bc
Abe <shalashtein@gmail.com>
2021-01-22 17:36:41 +0200

    revert to normal

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -160,7 +160,26 @@ var productSwipe = function(){
 const pcards = [].slice.call(document.querySelectorAll('#products-swipe-card-stack li'));
 
 // An instance of the Stack is used to attach event listeners.
-const pstack = Swing.Stack();
+const config = {
+  /**
+   * Invoked in the event of dragmove.
+   * Returns a value between 0 and 1 indicating the completeness of the throw out condition.
+   * Ration of the absolute distance from the original card position and element width.
+   *
+   * @param {number} xOffset Distance from the dragStart.
+   * @param {number} yOffset Distance from the dragStart.
+   * @param {HTMLElement} element Element.
+   * @returns {number}
+   */
+  throwOutConfidence: (xOffset, yOffset, element) => {
+    const xConfidence = Math.min(Math.abs(xOffset) / element.offsetWidth*1.5, 1);
+    const yConfidence = Math.min(Math.abs(yOffset) / element.offsetHeight, 1);
+
+    return Math.max(xConfidence, yConfidence);
+  }
+};
+
+const pstack = Swing.Stack(config);
 var pcurrentCard = "";
   pcards.forEach((targetElement) => {
     // Add card element to the Stack.
@@ -168,42 +187,49 @@ var pcurrentCard = "";
   });
 
   // Add event listener for when a card is thrown out of the stack.
-  pstack.on('throwout', (event) => {
+  pstack.on('throwout', (e) => {
     // e.target Reference to the element that has been thrown out of the stack.
     // e.throwDirection Direction in which the element has been thrown (Direction.LEFT, Direction.RIGHT).
-    event.target.classList.add("hidden");
+    e.target.classList.add("hidden");
     console.log('Card has been thrown out of the stack.');
-    console.log('Throw direction: ' + (event.throwDirection == Direction.LEFT ? 'left' : 'right'));
-    pstack.getCard(event.target).destroy()
+    console.log('Throw direction: ' + (e.throwDirection == Direction.LEFT ? 'left' : 'right'));
+    pstack.getCard(e.target).destroy()
   });
 
-  pstack.on('dragstart', (event) =>{
-    pcurrentCard = event.target;
+  pstack.on('dragstart', (e) =>{
+    pcurrentCard = e.target;
   });
-  pstack.on('dragmove', (event) => {
+  pstack.on('dragmove', (e) => {
     // e.target Reference to the element that has been thrown out of the stack.
     // e.throwDirection Direction in which the element has been thrown (Direction.LEFT, Direction.RIGHT).
-    console.log(event.target);
-    var d = event.throwDirection == Direction.LEFT ? 'left' : 'right'
+    console.log(e.target);
+    var d = e.throwDirection == Direction.LEFT ? 'left' : 'right'
     console.log('Yo this shit is moving to the ' + d);
+    console.log('Confidence = ' + e.throwOutConfidence)
+    var likeIcon = e.target.querySelector(".swipe-card-like");
+    var dislikeIcon = e.target.querySelector(".swipe-card-dislike");
     if(d == 'left'){
-      event.target.querySelector(".swipe-card-like").classList.add("hidden");
-      event.target.querySelector(".swipe-card-dislike").classList.remove("hidden");
+      likeIcon.classList.add("hidden");
+      dislikeIcon.classList.remove("hidden");
+      dislikeIcon.style.opacity = e.throwOutConfidence;
     }
     else{
-      event.target.querySelector(".swipe-card-like").classList.remove("hidden");
-      event.target.querySelector(".swipe-card-dislike").classList.add("hidden");
+      likeIcon.classList.remove("hidden");
+      likeIcon.style.opacity = e.throwOutConfidence;
+      dislikeIcon.classList.add("hidden");
     }
 
   });
-  pstack.on('dragend', (event) =>{
-    pcurrentCard = event.target;
+
+  pstack.on('dragend', (e) =>{
+    console.log("DRAG ENDED");
+    console.log(e);
   });
 
   // Add event listener for when a card is thrown in the stack, including the spring back into place effect.
   pstack.on('throwin', () => {
     console.log('Card has snapped back to the stack.');
-    currentCard.querySelector(".swipe-card-like").classList.add("hidden");
-    currentCard.querySelector(".swipe-card-dislike").classList.add("hidden");
+    pcurrentCard.querySelector(".swipe-card-like").classList.add("hidden");
+    pcurrentCard.querySelector(".swipe-card-dislike").classList.add("hidden");
   });
 };
