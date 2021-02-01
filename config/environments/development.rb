bb8a3ebab59715c395bc57146aba82c6c8277ef9
Abe <shalashtein@gmail.com>
2021-02-02 01:56:41 +0200

    SMTP credentials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,16 @@ Rails.application.configure do
   :bucket => ENV["BUCKET_NAME"]
   }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port: 587,
+    address: 'email-smtp.eu-west-1.amazonaws.com',
+    user_name: ENV["SES_SMTP_USERNAME"],
+    password: ENV["SES_SMTP_PASSWORD"],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
