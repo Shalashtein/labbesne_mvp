597c0614e990280dbee2c783cc0c0a36cd3fadf9
Abe <shalashtein@gmail.com>
2021-02-07 18:39:14 +0200

    x

--- app/javascript/packs/landing.js
+++ app/javascript/packs/landing.js
@@ -1,4 +1,35 @@
-(function () {
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("@fortawesome/fontawesome-free/js/all")
+require("channels")
+require("../scripts/landingSwipe.js")
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+
+
+// ----------------------------------------------------
+// Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
+// WRITE YOUR OWN JS STARTING FROM HERE 👇
+// ----------------------------------------------------
+
+// External imports
+import "bootstrap";
+const Swing = require("swing");
+require("swing");
+// Internal imports, e.g:
+// import { initSelect2 } from '../components/init_select2';
+
+document.addEventListener('turbolinks:load', () => {
   const doc = document
   const rootEl = doc.documentElement
   const body = doc.body
@@ -27,25 +58,6 @@
   if (body.classList.contains('has-animations')) {
     window.addEventListener('load', revealAnimations)
   }
-
-  // Light switcher
-  if (lightSwitch) {
-    window.addEventListener('load', checkLights)
-    lightSwitch.addEventListener('change', checkLights)
-  }
-
-  function checkLights () {
-    let labelText = lightSwitch.parentNode.querySelector('.label-text')
-    if (lightSwitch.checked) {
-      body.classList.remove('lights-off')
-      if (labelText) {
-        labelText.innerHTML = 'dark'
-      }
-    } else {
-      body.classList.add('lights-off')
-      if (labelText) {
-        labelText.innerHTML = 'light'
-      }
-    }
-  }
-}())
+  productSwipe();
+  dynamicShop();
+});
