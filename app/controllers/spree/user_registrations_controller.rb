9c085f10b3c02f36698d8744a36292fb3ebb8e38
God <shalashtein@gmail.com>
2021-02-19 22:28:34 +0200

    x

--- app/controllers/spree/user_registrations_controller.rb
+++ app/controllers/spree/user_registrations_controller.rb
@@ -8,45 +8,10 @@ class Spree::UserRegistrationsController < Devise::RegistrationsController
   include Spree::Core::ControllerHelpers::Order
   include Spree::Core::ControllerHelpers::Store
 
-  before_action :check_permissions, only: [:edit, :update]
-  skip_before_action :require_no_authentication
-
-  def create
-    build_resource(spree_user_params)
-    if resource.save
-      p = Profile.new
-      p.spree_user_id = resource.id
-      p.save!
-      set_flash_message(:notice, :signed_up)
-      if resource.confirmed_at.nil?
-        session[:user_id] = resource.id
-        respond_with resource, location: main_app.confirmation_path
-      else
-        sign_in(:spree_user, resource)
-        session[:spree_user_signup] = true
-        respond_with resource, location: main_app.root_path
-      end
-    else
-      clean_up_passwords(resource)
-      respond_with(resource) do |format|
-        format.html { render :new }
-      end
-    end
-  end
-
   protected
 
-  def translation_scope
-    'devise.user_registrations'
-  end
-
-  def check_permissions
-    authorize!(:create, resource)
-  end
-
-  private
-
-  def spree_user_params
-    params.require(:spree_user).permit(Spree::PermittedAttributes.user_attributes | [:email])
+  def after_confirmation_path_for(resource_name, resource)
+    sign_in(resource)
+    signed_in?(resource_name) ? spree.signed_in_root_path(resource) : spree.login_path
   end
 end
