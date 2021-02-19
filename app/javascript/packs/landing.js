c18ab789c591e4085688aece1697a513f45cf807
God <shalashtein@gmail.com>
2021-02-19 21:16:54 +0200

    ux confirmation

--- app/javascript/packs/landing.js
+++ app/javascript/packs/landing.js
@@ -30,33 +30,6 @@ require("swing");
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
-  const doc = document
-  const rootEl = doc.documentElement
-  const body = doc.body
-  const lightSwitch = doc.getElementById('lights-toggle')
-  /* global ScrollReveal */
-  const sr = window.sr = ScrollReveal()
-
-  rootEl.classList.remove('no-js')
-  rootEl.classList.add('js')
-
-  window.addEventListener('load', function () {
-    body.classList.add('is-loaded')
-  })
-
-  // Reveal animations
-  function revealAnimations () {
-    sr.reveal('.feature', {
-      duration: 600,
-      distance: '20px',
-      easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-      origin: 'right',
-      viewFactor: 0.2
-    })
-  }
-  if (body.classList.contains('has-animations')) {
-    window.addEventListener('load', revealAnimations)
-  }
   $(document).ready(function(){
     $('.hero-image').fadeIn(500, function(){});
     $('.dynamic-deck').fadeIn(500, function(){});
